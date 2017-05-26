28fff0475fdf4522997f4f4a54d7449b230db506
Pond <pond@getmii.com>
2017-05-26 12:33:46 +0700

    Generate Quiz controller

---
+++ app/controllers/quizzes_controller.rb
@@ -0,0 +1,7 @@
+class QuizzesController < ApplicationController
+  def index
+  end
+
+  def quiz_1
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'quizzes#index'
+  get 'quiz/1', to: 'quizzes#quiz_1'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
