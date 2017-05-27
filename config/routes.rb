2e2bed1fbcc13a3c580ba36361ff12ba3d410bda
Pond <pond@getmii.com>
2017-05-27 11:45:21 +0700

    done functional quiz 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'quizzes#index'
   get 'quiz/1', to: 'quizzes#quiz_1'
-
+  match 'quizzes/processing', to: 'quizzes#processing', via: :post
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
